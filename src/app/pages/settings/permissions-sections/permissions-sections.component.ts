7c9f081cc5feaf6a80395d0ad7d781fe29be1f7d
Di Louner <indiotoons@gmail.com>
2025-01-13 08:45:36 -0700

    permissions-sections.components.ts

--- src/app/pages/settings/permissions-sections/permissions-sections.component.ts
+++ src/app/pages/settings/permissions-sections/permissions-sections.component.ts
@@ -31,6 +31,7 @@ selectedUserType: string = 'all';
     private http: HttpClient,
     private toastrService: NbToastrService,
     private companyService: CompanyService,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
@@ -56,7 +57,175 @@ selectedUserType: string = 'all';
       );
   }
 
-  
+  async loadSections() {
+    const companyId = this.companyService.selectedCompany.Id;
+    const data = { companyId: companyId };
+
+    this.http.post('https://siinad.mx/php/loadCompanySections.php', data).subscribe(
+      async (response: any) => {
+        if (response.success) {
+          const allSections = ['Sistema REPSE', 'Control de proyectos', 'Empleados', 'Incidencias', 'Costos', 'Ventas', 'Configuracion de mi empresa', 'Configuracion de perfiles', 'Configuracion de socios comerciales', 'Configuracion de sitio', 'Configuracion de usuarios'];
+          const assignedSections = response.sections.map((section: { NameSection: string }) => section.NameSection);
+          this.sections = allSections.filter(section => assignedSections.includes(section))
+         
+        } else {
+          console.error(response.error);
+          await this.showToast(response.error, 'danger');
+        }
+      },
+      async (error) => {
+        console.error('Error en la solicitud POST:', error);
+        await this.showToast('Error al cargar secciones.', 'danger');
+      }
+    );
+  }
+
+  async onUserTypeChange(event: any) {
+    const userType = event.target.value;
+
+    if (userType === 'all') {
+      this.filteredUsers = this.users;
+    } else {
+      this.filteredUsers = this.users.filter(user => user.role === userType);
+    }
+  }
+
+  async onUserChange(event: any) {
+    this.selectedUserId = event.target.value;
+    await this.loadPermissions();
+  }
+
+  async onSectionChange(event: any) {
+    this.selectedSection = event.target.value;
+    this.loadSubSections(this.selectedSection);
+  }
+
+  async loadSubSections(section: string) {
+    const subSectionsMap: { [key: string]: string[] } = {
+      'Sistema REPSE': [''],
+      'Control de proyectos': [
+        'Asignacion de proyectos',
+        'Registro de proyectos',
+        'Vizualizar proyectos', 
+        'Seguimiento de proyectos'
+      ],
+      'Empleados': ['Registrar solicitudes de empleados', 'Editar solicitudes de empleados', 'Aceptar solicitudes de empleados', 'Procesar empleados', 'Ver empleados registrados'],
+      'Incidencias': ['Control de incidencias', 'Confirmar dia', 'Confirmar semana', 'Semanas procesadas', 'Lista de asistencia'],
+      'Costos': [''],
+      'Ventas': [''],
+     'Configuracion de mi empresa': [
+        'Asignar logo de la empresa',
+        'Código de la empresa',
+        'Departamentos',
+        'Configuración inicial de períodos',
+        'Tipos de período',
+        'Catálogo de períodos'
+      ],
+      'Configuracion de perfiles': [''],
+      'Configuracion de socios comerciales': [
+        'Autorizar socio comercial',
+        'Editar roles de los socios comerciales',
+        'Registrar socio comercial',
+        'Secciones visibles de los socios comerciales'
+      ],
+      'Configuracion de sitio': [
+        'Secciones visibles de empresas',
+        'Empresas registradas en la página',
+        'Registrar empresas',
+        'Confirmar solicitudes premium'
+      ],
+      'Configuracion de usuarios': [
+        'Registrar, eliminar, ver y editar usuarios',
+        'Editar mi usuario'
+      ],
+    };
+
+    const subSectionsProviderMap: { [key: string]: string[] } = {
+      'Sistema REPSE': [''],
+      'Control de proyectos': [
+        'Asignacion de proyectos',
+        'Registro de proyectos',
+        'Vizualizar proyectos', 
+        'Seguimiento de proyectos'
+      ],
+      'Empleados': ['Registrar solicitudes de empleados', 'editar solicitudes de empleados', 'Ver empleados registrados'],
+      'Incidencias': ['Control de incidencias', 'Confirmar dia', 'Confirmar semana', 'Semanas procesadas', 'Lista de asistencia'],
+      'Costos': [''],
+      'Ventas': [''],
+     'Configuracion de mi empresa': [
+        'Asignar logo de la empresa',
+        'Código de la empresa',
+        'Departamentos',
+        'Configuración inicial de períodos',
+        'Tipos de período',
+        'Catálogo de períodos'
+      ],
+      'Configuracion de perfiles': [''],
+      'Configuracion de socios comerciales': [
+        'Autorizar socio comercial',
+        'Editar roles de los socios comerciales',
+        'Registrar socio comercial',
+        'Secciones visibles de los socios comerciales'
+      ],
+      'Configuracion de sitio': [
+        'Secciones visibles de empresas',
+        'Empresas registradas en la página',
+        'Registrar empresas',
+        'Confirmar solicitudes premium'
+      ],
+      'Configuracion de usuarios': [
+        'Registrar, eliminar, ver y editar usuarios',
+        'Editar mi usuario'
+      ],
+    };
+
+    const subSectionsClientMap: { [key: string]: string[] } = {
+      'Sistema REPSE': [''],
+      'Control de proyectos': [
+        'Asignacion de proyectos',
+        'Registro de proyectos',
+        'Vizualizar proyectos', 
+        'Seguimiento de proyectos'
+      ],
+     'Empleados': ['Registrar solicitudes de empleados', 'editar solicitudes de empleados', 'Ver empleados registrados'],
+      'Incidencias': ['Control de incidencias', 'Confirmar dia', 'Confirmar semana', 'Semanas procesadas', 'Lista de asistencia'],
+      'Costos': [''],
+      'Ventas': [''],
+      'Configuracion de mi empresa': [
+        'Asignar logo de la empresa',
+        'Código de la empresa',
+        'Departamentos',
+        'Configuración inicial de períodos',
+        'Tipos de período',
+        'Catálogo de períodos'
+      ],
+      'Configuracion de perfiles': [''],
+      'Configuracion de socios comerciales': [
+        'Autorizar socio comercial',
+        'Editar roles de los socios comerciales',
+        'Registrar socio comercial',
+        'Secciones visibles de los socios comerciales'
+      ],
+      'Configuracion de sitio': [
+        'Secciones visibles de empresas',
+        'Empresas registradas en la página',
+        'Registrar empresas',
+        'Confirmar solicitudes premium'
+      ],
+      'Configuracion de usuarios': [
+        'Registrar, eliminar, ver y editar usuarios',
+        'Editar mi usuario'
+      ],
+    };
+
+    this.subSections = subSectionsMap[section] || [];
+
+    if (this.companyService.selectedCompany.Role === 'proveedor') {
+      this.subSectionsProvider = subSectionsProviderMap[section] || [];
+    } else if (this.companyService.selectedCompany.Role === 'cliente') {
+      this.subSectionsClient = subSectionsClientMap[section] || [];
+    }
+  }
 
   loadPermissions() {
     if (!this.selectedUserId) return;
@@ -162,5 +331,9 @@ selectedUserType: string = 'all';
 
   showToast(message: string, status: 'success' | 'danger') {
     this.toastrService.show(message, 'Notificación', { status });
+  }
+
+  goBack() {
+    this.router.navigate(['/previous-page']);  // Puedes definir la ruta que necesites
   }
 }
