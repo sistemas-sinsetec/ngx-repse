69f0dc62ddaf0d6926f3311b4f02719f7d467c86
Di Louner <indiotoons@gmail.com>
2025-01-15 14:51:02 -0700

    import de component y module incident-control3

--- src/app/pages/incident-control/incident-control.module.ts
+++ src/app/pages/incident-control/incident-control.module.ts
@@ -1,5 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  NbButtonModule,
+  NbCardModule,
+  NbIconModule,
+  NbAlertModule,
+  NbToastrModule,
+  NbListModule,
+  NbCheckboxModule,
+  NbCalendarKitModule,
+  NbCalendarModule,
+  NbCalendarRangeModule,
+  NbSelectModule,
+  NbOptionModule,
+  NbDatepickerModule,
+  NbBadgeModule, // Importa NbBadgeModule aquí
+  NbLayoutModule, // Importa NbLayoutModule si usas nb-layout
+  NbFormFieldModule,
+  NbAccordionModule,
+  NbInputModule,
+  NbTagModule,
+  NbUserModule
+} from '@nebular/theme';
+
+import { IncidentControlRoutingModule } from './incident-control-routing.module';
+import { IncidentControlComponent } from './incident-control.component';
+
 
 
 
