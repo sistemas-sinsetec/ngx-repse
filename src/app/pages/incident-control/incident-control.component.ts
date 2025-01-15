9916a8187a34ee80a98b2d35d57f3626a76e99db
Di Louner <indiotoons@gmail.com>
2025-01-15 12:54:32 -0700

    creacion routing, module y component incident-control

---
+++ src/app/pages/incident-control/incident-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-incident-control',
+  templateUrl: './incident-control.component.html',
+  styleUrls: ['./incident-control.component.scss']
+})
+export class IncidentControlComponent {
+
+}
---
+++ src/app/pages/incident-control/incident-control.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IncidentControlComponent } from './incident-control.component';
+
+
+
+@NgModule({
+  declarations: [
+    IncidentControlComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class IncidentControlModule { }
