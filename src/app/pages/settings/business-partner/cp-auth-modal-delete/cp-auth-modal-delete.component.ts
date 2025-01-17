f3fe38a174d0072d3daeea7a0bd9652c0a11262f
Di Louner <indiotoons@gmail.com>
2025-01-17 10:10:58 -0700

    modales ts de cp-auth

--- src/app/pages/settings/business-partner/cp-auth-modal-delete/cp-auth-modal-delete.component.ts
+++ src/app/pages/settings/business-partner/cp-auth-modal-delete/cp-auth-modal-delete.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NbDialogRef } from '@nebular/theme';
+
 
 @Component({
   selector: 'ngx-cp-auth-modal-delete',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./cp-auth-modal-delete.component.scss']
 })
 export class CpAuthModalDeleteComponent {
+  @Input() mostrarMotivoRechazo: boolean;
+  @Input() confirmarRechazoSocio: Function; // Definir la entrada para la función confirmarRechazoSocio
+  @Output() motivoRechazoConfirmado = new EventEmitter<string>();
 
+  continuarRegistro: boolean;
+
+  motivo: string = '';
+
+  constructor(private dialogRef: NbDialogRef<CpAuthModalDeleteComponent>) { }
+
+  confirmarRechazo() {
+    console.log('Se confirmó el rechazo con motivo:', this.motivo);
+    // Emitir el motivo y cerrar el diálogo
+    this.motivoRechazoConfirmado.emit(this.motivo);
+    this.dialogRef.close(this.motivo); // Enviar el motivo al cerrar el modal
+  }
+
+  cancelar() {
+    console.log('Se canceló el rechazo');
+    this.dialogRef.close(null); // Cerrar el diálogo sin motivo
+  }
 }
